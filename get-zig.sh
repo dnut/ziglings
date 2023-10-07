8a824412cabd8c8eb154ef43263d6366c9d711e4
Drew Nutter <dnut@users.noreply.github.com>
2023-10-07 12:04:50 -0300

    optionally remove old toolchains with get-zig.sh

--- get-zig.sh
+++ get-zig.sh
@@ -2,9 +2,12 @@
 set -euxo pipefail
 
 os=linux
+remove_others=true
+
 arch=$(uname -m)
 version=$(curl https://ziglang.org/download/index.json | jq -r .master.version)
-archive="zig-${os}-${arch}-${version}.tar.xz"
+name="zig-${os}-${arch}-${version}"
+archive="${name}.tar.xz"
 pubkey=RWSGOq2NVecA2UPNdBUZykf1CCb147pkmdtYxgb3Ti+JO/wCYvhbAb/U
 
 mkdir -p toolchain/download
@@ -24,7 +27,24 @@ check-download || {
     check-download
 }
 
+if [[ "$remove_others" == true ]]; then
+    find toolchain/download/ -maxdepth 1 -mindepth 1 | while read path; do
+        if [[ "$path" != "toolchain/download/$archive" ]] && \
+            [[ "$path" != "toolchain/download/${archive}.minisig" ]]; then
+            rm "$path"
+        fi
+    done
+fi
+
 tar -C toolchain/versions -xf toolchain/download/${archive}
 
 rm -f zig
-ln -s toolchain/versions/zig-${os}-${arch}-${version}/zig zig
+ln -s toolchain/versions/${name}/zig zig
+
+if [[ "$remove_others" == true ]]; then
+    find toolchain/versions/ -maxdepth 1 -mindepth 1 | while read path; do
+        if [[ "$path" != "toolchain/versions/$name" ]]; then
+            rm -rf "$path"
+        fi
+    done
+fi
